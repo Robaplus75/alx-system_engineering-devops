724aa8eb9c71ebe456d6aae60411b35c4e1e127b
robel <robelwon75@gmail.com>
2023-11-15 21:39:19 +0300

    task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# changes the os configuration so that it is possible to login wit the holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+# increases the soft file limit for holberton user
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
